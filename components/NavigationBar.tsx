49c2fb2a0ea527a66fb6563588def75e12fd94e1
Panagiotis Mentzelopoulos <panosmentz@gmail.com>
2023-02-09 21:28:50 +0000

    Add navbar animation and refactor on small devices

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -19,33 +19,27 @@ const NavigationBar = () => {
         <div className="hidden md:block">
           <Link
             href="/keyboards"
-            className="text-white text-l mr-6 font-bold text-underline transition-colors duration-300 hover:text-underline-current hover:bg-teal-900"
+            className="text-white mr-6 px-3 py-3 hover-underline-animation"
           >
             Keyboards
           </Link>
           <Link
             href="/mice"
-            className="text-white text-l mr-6 font-bold text-underline transition-colors duration-300 hover:text-underline-current hover:bg-teal-900"
+            className="text-white mr-6 px-3 py-3 hover-underline-animation"
           >
             Mice
           </Link>
           <Link
             href="/headsets"
-            className="text-white text-l mr-6 font-bold text-underline transition-colors duration-300 hover:text-underline-current hover:bg-teal-900"
+            className="text-white mr-6 px-3 py-3 hover-underline-animation"
           >
             Headsets
           </Link>
           <Link
             href="/mousepads"
-            className="text-white text-l mr-6 font-bold text-underline transition-colors duration-300 hover:text-underline-current hover:bg-teal-900"
+            className="text-white mr-6 px-3 py-3 hover-underline-animation"
           >
             Mousepads
-          </Link>
-          <Link
-            href="/contact"
-            className="text-white text-l mr-6 font-bold text-underline transition-colors duration-300 hover:text-underline-current hover:bg-teal-900"
-          >
-            Contact
           </Link>
         </div>
 
@@ -70,43 +64,66 @@ const NavigationBar = () => {
         <div
           className={`${
             isOpen ? "block" : "hidden"
-          } md:hidden absolute right-0 top-0 mt-16 w-full  py-2 bg-white rounded-lg shadow-xl`}
+          } md:hidden absolute right-0 top-0 w-full h-full py-2 bg-black rounded-lg shadow-xl flex justify-center align-center`}
         >
-          <Link
-            href="/keyboards"
-            className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-            onClick={() => setIsOpen(false)}
-          >
-            Keyboards
-          </Link>
-          <Link
-            href="/mice"
-            className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-            onClick={() => setIsOpen(false)}
-          >
-            Mice
-          </Link>
-          <Link
-            href="/headsets"
-            className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-            onClick={() => setIsOpen(false)}
-          >
-            Headsets
-          </Link>
-          <Link
-            href="/mousepads"
-            className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-            onClick={() => setIsOpen(false)}
-          >
-            Mousepads
-          </Link>
-          <Link
-            href="/contact"
-            className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-            onClick={() => setIsOpen(false)}
-          >
-            Contact
-          </Link>
+          <div className="mt-16">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={`md:hidden rounded-full focus:outline-none focus:shadow-outline-blue absolute right-10 top-10`}
+            >
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <div className="flex flex-col items-center ">
+              <Link
+                href="/home"
+                className="px-4 py-2 hover-underline-animation"
+                onClick={() => setIsOpen(false)}
+              >
+                Home
+              </Link>
+              <Link
+                href="/keyboards"
+                className="px-4 py-2 hover-underline-animation"
+                onClick={() => setIsOpen(false)}
+              >
+                Keyboards
+              </Link>
+              <Link
+                href="/mice"
+                className="px-4 py-2 hover-underline-animation"
+                onClick={() => setIsOpen(false)}
+              >
+                Mice
+              </Link>
+              <Link
+                href="/headsets"
+                className="px-4 py-2 hover-underline-animation"
+                onClick={() => setIsOpen(false)}
+              >
+                Headsets
+              </Link>
+              <Link
+                href="/mousepads"
+                className="px-4 py-2 hover-underline-animation"
+                onClick={() => setIsOpen(false)}
+              >
+                Mousepads
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
